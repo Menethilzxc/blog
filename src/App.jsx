04bc2566172fcfda8e2d81b644610257b4f203bf
Dmitry Tupikin <dilondd11@gmail.com>
2025-01-11 01:07:43 +0300

    BLOG-003

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+`;
+
+function App() {
+	return (
+		<Div>
+			<div>123</div>
+			<div>
+				<i className="fa fa-gamepad"></i>
+			</div>
+		</Div>
+	);
+}
+
+export default App;
