cead20c9bde6c0cc629d555dbdf2be073aebd4bd
Dmitry Tupikin <dilondd11@gmail.com>
2025-02-11 19:57:34 +0300

    Реализовать страницу Главная

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -3,7 +3,7 @@ import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 import { Header, Footer, Modal } from './components';
 import { setUser } from './actions';
-import { Authorization, Registration, Users, Post } from './pages';
+import { Authorization, Registration, Users, Post, Main } from './pages';
 
 import styled from 'styled-components';
 
@@ -46,7 +46,7 @@ function Blog() {
 			<Header />
 			<Page>
 				<Routes>
-					<Route path="/" element={<div>Главная страница</div>} />
+					<Route path="/" element={<Main />} />
 					<Route path="/login" element={<Authorization />} />
 					<Route path="/register" element={<Registration />} />
 					<Route path="/users" element={<Users />} />
